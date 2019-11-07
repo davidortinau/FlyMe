2ca79bd0b9acb91a0e89864414932ca1cf6c4165
David Ortinau <david.ortinau@microsoft.com>
2019-11-07 09:45:55 -0500

    added search handler

--- FlyMe/FlyMe/FlightResultsPage.xaml.cs
+++ FlyMe/FlyMe/FlightResultsPage.xaml.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using FlyMe.ViewModels;
+using System;
 using System.Collections.Generic;
-
+using System.Linq;
 using Xamarin.Forms;
 
 namespace FlyMe
@@ -10,6 +11,39 @@ namespace FlyMe
         public FlightResultsPage()
         {
             InitializeComponent();
+
+            Shell.SetSearchHandler(this, new FeedSearchHandler(vm));
+        }
+    }
+
+    public class FeedSearchHandler : SearchHandler
+    {
+        FlightResultsViewModel _vm;
+        public FeedSearchHandler(ViewModels.FlightResultsViewModel vm)
+        {
+            SearchBoxVisibility = SearchBoxVisibility.Expanded;
+            IsSearchEnabled = true;
+            ShowsResults = false;
+            Placeholder = "Find a Flight";
+            CancelButtonColor = Color.DarkBlue;
+            TextColor = Color.DarkBlue;
+
+            _vm = vm;
+        }
+
+        protected override void OnQueryChanged(string oldValue, string newValue)
+        {
+            base.OnQueryChanged(oldValue, newValue);
+            if (string.IsNullOrEmpty(newValue))
+            {
+                ItemsSource = null;
+            }
+            else
+            {
+                var results = new List<string>();
+                results = _vm.Flights.Where(x => x.Price.IndexOf(newValue, StringComparison.InvariantCultureIgnoreCase) > -1).Select(x => $"{x.From} to {x.To} - {x.Price}").ToList<string>();
+                ItemsSource = results;
+            }
         }
     }
 }
