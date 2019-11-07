3c295c6f571269012071b2e7e567e8981e9bf8ba
David Ortinau <david.ortinau@microsoft.com>
2019-11-07 07:22:30 -0500

    threshold loading incremental

--- FlyMe/FlyMe/ViewModels/FlightResultsViewModel.cs
+++ FlyMe/FlyMe/ViewModels/FlightResultsViewModel.cs
@@ -1,13 +1,39 @@
-﻿using System;
+﻿using MvvmHelpers;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Xamarin.Forms;
 
 namespace FlyMe.ViewModels
 {
-    public class FlightResultsViewModel
+    public class FlightResultsViewModel : BaseViewModel
     {
+        public Command LoadMoreFlightsCommand { get; set; }
         public FlightResultsViewModel()
         {
+            LoadMoreFlightsCommand = new Command(LoadMore);
+
             InitData();
+        }
+
+        int batchSize = 10;
+        int currentFlightIndex = 0;
+
+        void LoadMore()
+        {
+            FlightsToDisplay.AddRange(
+                Flights.Skip(batchSize * currentFlightIndex).Take(batchSize)
+            );
+            currentFlightIndex += batchSize;
+        }
+
+        public ObservableRangeCollection<Flight> FlightsToDisplay
+        {
+            get => flightsToDisplay; 
+            set
+            {
+                SetProperty(ref flightsToDisplay, value);
+            }
         }
 
         public List<Flight> Flights { get; set; }
@@ -17,17 +43,19 @@ namespace FlyMe.ViewModels
         private void InitData()
         {
             Flights = new List<Flight>();
-            for(var i = 1; i <= 1000; i++)
+            for (var i = 1; i <= 1000; i++)
             {
                 Flights.Add(new Flight
                 {
-                    From = cities[random.Next(cities.Length-1)],
+                    From = cities[random.Next(cities.Length - 1)],
                     To = cities[random.Next(cities.Length - 1)],
-                    DepartDateTime = departDates[random.Next(departDates.Length-1)],
+                    DepartDateTime = departDates[random.Next(departDates.Length - 1)],
                     ArrivalDateTime = arrivalDates[random.Next(arrivalDates.Length - 1)],
-                    Price = prices[random.Next(prices.Length-1)]
+                    Price = prices[random.Next(prices.Length - 1)]
                 });
             }
+
+            FlightsToDisplay = new ObservableRangeCollection<Flight>(Flights.Take(batchSize).ToList());
         }
 
         private string[] cities = new string[]
@@ -59,6 +87,7 @@ namespace FlyMe.ViewModels
         {
             "$500", "$1,200", "$350", "$750", "$1,250", "$200"
         };
+        private ObservableRangeCollection<Flight> flightsToDisplay;
     }
 
     public class Flight
