fbbf7ae8fee518cabeca948a21ba9f91e6caa30c
David Ortinau <david.ortinau@microsoft.com>
2019-11-13 16:36:18 -0600

    fixed splash screen

---
+++ FlyMe/FlyMe.Android/Renderers/FlyShellRenderer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace FlyMe.Droid.Renderers
+{
+    public class FlyShellRenderer
+    {
+        public FlyShellRenderer()
+        {
+        }
+
+        //Device.BeginInvokeOnMainThread(() =>
+        //    {
+        //        var parent = (AppBarLayout)toolbar.Parent;
+        //parent.SetElevation(0);
+        //    });
+    }
+}
---
+++ FlyMe/FlyMe.iOS/LauncherViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace FlyMe.iOS
+{
+    public partial class LauncherViewController : UIViewController
+    {
+        public LauncherViewController() : base("LauncherViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
