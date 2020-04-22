46bd66b8272af2de697d80f534dfde28b8676b22
David Ortinau <david.ortinau@microsoft.com>
2020-04-21 20:37:47 -0500

    added bag tracker

---
+++ FlyMe/FlyMe/ViewModels/BagTrackerViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MvvmHelpers;
+using Xamarin.Forms;
+
+namespace FlyMe.ViewModels
+{
+    public class BagTrackerViewModel : BaseViewModel
+    {
+        public Command SearchCommand { get; internal set; }
+
+        public BagTrackerViewModel()
+        {
+            SearchCommand = new Command(OnSearch);
+        }
+
+        private void OnSearch()
+        {
+            App.Current.MainPage.DisplayAlert("Bags Found!", "They are in your closet because you're staying at home!", "Thanks!");
+        }
+    }
+}
+
