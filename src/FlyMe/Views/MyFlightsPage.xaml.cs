1028d7cc9a60cbf828d53388d0bbcc017de38e79
David Ortinau <davidortinau@Davids-MBP.localdomain>
2022-06-12 20:19:07 -0500

    start of comparison images and notes

--- src/FlyMe/Views/MyFlightsPage.xaml.cs
+++ src/FlyMe/Views/MyFlightsPage.xaml.cs
@@ -17,11 +17,11 @@ namespace FlyMe.Views
             base.OnAppearing();
 
             // this is the sizing that crashes the view
-            var inAnims = new Animation();
-            inAnims.Add(0, 1, new Animation(v => this.MilesProgressBar.WidthRequest = Math.Max(0,v), 0, 200, Easing.SpringIn));
-            inAnims.Add(0.25, 1, new Animation(v => this.SegmentsProgressBar.WidthRequest = Math.Max(0,v), 0, 100, Easing.SpringIn));
+            // var inAnims = new Animation();
+            // inAnims.Add(0, 1, new Animation(v => this.MilesProgressBar.WidthRequest = Math.Max(0,v), 0, 200, Easing.SpringIn));
+            // inAnims.Add(0.25, 1, new Animation(v => this.SegmentsProgressBar.WidthRequest = Math.Max(0,v), 0, 100, Easing.SpringIn));
 
-            inAnims.Commit(this, "grow_bar", 16, 2000);
+            // inAnims.Commit(this, "grow_bar", 16, 2000);
         }
 
         void Button_Clicked(System.Object sender, System.EventArgs e)
